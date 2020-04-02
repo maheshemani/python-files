6a26c183709f5b2b5c8f150f6b123563aeaa6535
maheshemani <42319674+maheshemani@users.noreply.github.com>
2020-04-02 15:33:09 +0530

    Added comments

--- inheritance.py
+++ inheritance.py
@@ -4,11 +4,12 @@ Created on Fri Jan 24 16:56:55 2020
 
 @author: mahesh.emani
 """
-
+#parent class
 class A:
     def testa(self):
         print("in A")
-        
+
+#child class        
 class B(A):
     def testb(self):
         print("in B")
@@ -17,6 +18,7 @@ b=B()
 b.testa()
 b.testb()
 
+#grand child
 class C(B):
     def testc(self):
         print("in C")
